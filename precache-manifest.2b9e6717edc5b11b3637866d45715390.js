cf6aeed929f1c0f7f48266ed8125abb9ab0f8c44
2b0f7bbd <est@localhost.localdomain>
2020-01-07 14:08:49 +0500

    Updates

---
+++ precache-manifest.2b9e6717edc5b11b3637866d45715390.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54379bc0305c379f69befaf04fe7eff8",
+    "url": "/weather_react_app/index.html"
+  },
+  {
+    "revision": "a1db7ff1db57feb557aa",
+    "url": "/weather_react_app/static/css/main.025526d2.chunk.css"
+  },
+  {
+    "revision": "35dba0a56088e9e60e7d",
+    "url": "/weather_react_app/static/js/2.3cd96e40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/weather_react_app/static/js/2.3cd96e40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a1db7ff1db57feb557aa",
+    "url": "/weather_react_app/static/js/main.d3d8a6e9.chunk.js"
+  },
+  {
+    "revision": "a9dd7e5e49c6f524f669",
+    "url": "/weather_react_app/static/js/runtime-main.9249dd52.js"
+  },
+  {
+    "revision": "53f709c56c0cb268362ba119c34e072a",
+    "url": "/weather_react_app/static/media/clear-day.53f709c5.svg"
+  },
+  {
+    "revision": "b87ca8b626b1d792e4b35da43f795ba7",
+    "url": "/weather_react_app/static/media/clear-night.b87ca8b6.svg"
+  },
+  {
+    "revision": "79b3c647bcb4502fe1f95e9d9c5de5d9",
+    "url": "/weather_react_app/static/media/cloudy.79b3c647.svg"
+  },
+  {
+    "revision": "788adf71ba1fe5a50e21db06e8c99153",
+    "url": "/weather_react_app/static/media/fog.788adf71.svg"
+  },
+  {
+    "revision": "534afba6dc879efc03f27d5c964416bf",
+    "url": "/weather_react_app/static/media/partly-cloudy-day.534afba6.svg"
+  },
+  {
+    "revision": "316ec3310f84545f10f9525b44509d10",
+    "url": "/weather_react_app/static/media/rain.316ec331.svg"
+  },
+  {
+    "revision": "f4e14d44ac01a70d61f1cb77c4eebe21",
+    "url": "/weather_react_app/static/media/sleet.f4e14d44.svg"
+  },
+  {
+    "revision": "35404d3acb64aeb0eb68d9da2e81f3d7",
+    "url": "/weather_react_app/static/media/snow.35404d3a.svg"
+  },
+  {
+    "revision": "6b4d10062fb150d93eb6d0a550d19947",
+    "url": "/weather_react_app/static/media/wind.6b4d1006.svg"
+  }
+]);
